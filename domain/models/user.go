bc93cb8b176276f67a413701b73945188f1acb26
rangguy <ranggadwi100@gmail.com>
2025-04-23 22:36:13 +0700

    Setup Seeder

--- domain/models/user.go
+++ domain/models/user.go
@@ -9,6 +9,7 @@ type User struct {
 	ID          uint      `gorm:"primaryKey;autoIncrement"`
 	UUID        uuid.UUID `gorm:"type:uuid;not null"`
 	Name        string    `gorm:"type:varchar(100);not null"`
+	Username    string    `gorm:"type:varchar(20);not null"`
 	Password    string    `gorm:"type:varchar(255);not null"`
 	PhoneNumber string    `gorm:"type:varchar(15);not null"`
 	Email       string    `gorm:"type:varchar(100);not null"`
