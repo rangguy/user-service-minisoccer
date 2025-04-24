b559d83255d783a925627b300859627d28cc9cf3
rangguy <ranggadwi100@gmail.com>
2025-04-24 22:16:13 +0700

    Repository Bagian 1

--- common/error/error.go
+++ common/error/error.go
@@ -58,7 +58,7 @@ func ErrValidationResponse(err error) (validationResponse []ValidationResponse) 
 	return validationResponse
 }
 
-func wrapError(err error) error {
+func WrapError(err error) error {
 	logrus.Errorf("error: %v", err)
 	return err
 }
