f7b791cb3d1ceec976f787bb8d881cca21dc392b
rangguy <ranggadwi100@gmail.com>
2025-05-03 22:00:04 +0700

    Middleware Bagian 2: validateAPIKey

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -1,12 +1,17 @@
 package middlewares
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"github.com/didip/tollbooth"
 	"github.com/didip/tollbooth/limiter"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	"strings"
 	"user-service/common/response"
+	"user-service/config"
 	"user-service/constants"
 	errConstant "user-service/constants/error"
 )
@@ -39,4 +44,38 @@ func RateLimiter(lmt *limiter.Limiter) gin.HandlerFunc {
 		}
 		c.Next()
 	}
+}
+
+func extractBearerToken(token string) string {
+	arrayToken := strings.Split(token, " ")
+	if len(arrayToken) == 2 {
+		return arrayToken[1]
+	}
+	return ""
+}
+
+func responseUnauthorized(c *gin.Context, message string) {
+	c.JSON(http.StatusUnauthorized, response.Response{
+		Status:  constants.Error,
+		Message: message,
+	})
+	c.Abort()
+}
+
+func validateAPIKey(c *gin.Context) error {
+	apiKey := c.GetHeader(constants.XApiKey)
+	requestAt := c.GetHeader(constants.XRequestAt)
+	serviceName := c.GetHeader(constants.XServiceName)
+	signatureKey := config.Config.SignatureKey
+
+	validateKey := fmt.Sprintf("%s %s %s", serviceName, signatureKey, requestAt)
+	hash := sha256.New()
+	hash.Write([]byte(validateKey))
+	resultHashed := hex.EncodeToString(hash.Sum(nil))
+
+	if apiKey != resultHashed {
+		return errConstant.ErrUnauthorized
+	}
+
+	return nil
 }
