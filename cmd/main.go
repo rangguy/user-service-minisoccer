e98454ab438cb3f2e8130ec9e7d54af909a8e662
rangguy <ranggadwi100@gmail.com>
2025-05-04 18:24:39 +0700

    Main.go Bagian 2

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,23 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
+	"net/http"
 	"time"
+	"user-service/common/response"
 	"user-service/config"
+	"user-service/constants"
 	"user-service/controllers"
 	"user-service/database/seeders"
 	"user-service/domain/models"
+	"user-service/middlewares"
 	"user-service/repositories"
+	"user-service/routes"
 	"user-service/services"
 )
 
@@ -41,5 +50,47 @@ var command = &cobra.Command{
 		service := services.NewServiceRegistry(repository)
 		controller := controllers.NewControllerRegistry(service)
 
+		router := gin.Default()
+		router.Use(middlewares.HandlePanic())
+		router.NoRoute(func(c *gin.Context) {
+			c.JSON(http.StatusNotFound, response.Response{
+				Status:  constants.Error,
+				Message: fmt.Sprintf("Path %s", http.StatusText(http.StatusNotFound)),
+			})
+		})
+		router.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, response.Response{
+				Status:  constants.Success,
+				Message: "Welcome to User Service",
+			})
+		})
+		router.Use(func(c *gin.Context) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-service-name, x-api-key, x-request-at")
+			c.Next()
+		})
+
+		lmt := tollbooth.NewLimiter(
+			config.Config.RateLimiterMaxRequest,
+			&limiter.ExpirableOptions{
+				DefaultExpirationTTL: time.Duration(config.Config.RateLimiterTimeSecond) * time.Second,
+			},
+		)
+		router.Use(middlewares.RateLimiter(lmt))
+
+		group := router.Group("/api/v1")
+		route := routes.NewRouteRegistry(controller, group)
+		route.Serve()
+
+		port := fmt.Sprintf(":%d", config.Config.Port)
+		router.Run(port)
 	},
+}
+
+func Run() {
+	err := command.Execute()
+	if err != nil {
+		panic(err)
+	}
 }
