6d120071f4134ed3020391c89d390d0a56adec7f
rangguy <ranggadwi100@gmail.com>
2025-05-04 09:32:24 +0700

    Setup Route

---
+++ routes/registry.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"user-service/controllers"
+	routes "user-service/routes/user"
+)
+
+type Registry struct {
+	controller controllers.IControllerRegistry
+	group      *gin.RouterGroup
+}
+
+type IRouterRegistry interface {
+	Serve()
+}
+
+func NewRouteRegistry(controller controllers.IControllerRegistry, group *gin.RouterGroup) *Registry {
+	return &Registry{
+		controller: controller,
+		group:      group,
+	}
+}
+
+func (r *Registry) Serve() {
+	r.userRoute().Run()
+}
+
+func (r *Registry) userRoute() routes.IUserRoute {
+	return routes.NewUserRoute(r.controller, r.group)
+}
